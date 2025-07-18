fe4d31aedb6b13a5539fedbc1d534363734c2a69
puyantao@xurongwangluo.com <PyT@123759>
2025-07-18 15:57:43 +0800

    # Android  接入 flutter boost

---
+++ androidFlutter/app/src/main/java/com/example/androidflutter/MyApplication.java
@@ -0,0 +1,40 @@
+package com.example.androidflutter;
+
+import android.app.Application;
+import android.content.pm.ApplicationInfo;
+
+import com.example.androidflutter.router.MyFlutterBoostDelegate;
+import com.idlefish.flutterboost.FlutterBoost;
+import com.idlefish.flutterboost.FlutterBoostSetupOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //经过实验得出如下结论： pushNativeRoute 不是通过StartActivity过来的，而是从Flutter页面过来的
+        //在Activity中通过 FlutterBoost.instance().open(options); 打开的页面是Flutter页面，不是原生界面
+
+
+        ArrayList<String> args = new ArrayList<>();
+        args.add("--trace-systrace");
+        args.add("--user-authorization-code=QZvoUptODA+KDgeFUluhheYns7X7CnDu9YRv8YmU0GXQcKLzs4C2WgjblrAIhtkqqGg==");
+        boolean isDebugMode = (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+        List<String> entrypointArgs = new ArrayList<>();
+        entrypointArgs.add("I am from Android!");
+        entrypointArgs.add("--for-test");
+        FlutterBoostSetupOptions options = new FlutterBoostSetupOptions.Builder()
+                .dartEntrypointArgs(entrypointArgs)
+                .isDebugLoggingEnabled(isDebugMode)
+                .shellArgs(args.toArray(new String[0]))
+                .build();
+        FlutterBoost.instance().setup(this, new MyFlutterBoostDelegate(), engine -> {
+            // Register the platform view
+            engine.getPlugins();
+        }, options);
+    }
+
+}
---
+++ androidFlutter/app/src/main/java/com/example/androidflutter/router/MyFlutterBoostDelegate.java
@@ -0,0 +1,46 @@
+package com.example.androidflutter.router;
+
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.example.androidflutter.SimpleNativeActivity;
+import com.idlefish.flutterboost.FlutterBoost;
+import com.idlefish.flutterboost.FlutterBoostDelegate;
+import com.idlefish.flutterboost.FlutterBoostRouteOptions;
+import com.idlefish.flutterboost.containers.FlutterBoostActivity;
+
+import io.flutter.embedding.android.FlutterActivityLaunchConfigs;
+import io.flutter.embedding.android.FlutterActivityLaunchConfigs.BackgroundMode;
+
+public class MyFlutterBoostDelegate implements FlutterBoostDelegate {
+
+    @Override
+    public void pushNativeRoute(FlutterBoostRouteOptions options) {
+        System.out.println("pushNativeRoute " + options.pageName());
+        //从 Flutter 打开Native 页面，具体打开哪个页面 是根据 options.pageName 来决定的
+        //这里根据options.pageName来判断你想跳转哪个页面，这里简单给一个
+        Intent intent = new Intent(FlutterBoost.instance().currentActivity(), SimpleNativeActivity.class);
+        FlutterBoost.instance().currentActivity().startActivityForResult(intent, options.requestCode());
+    }
+
+    @Override
+    public void pushFlutterRoute(FlutterBoostRouteOptions options) {
+        System.out.println("pushFlutterRoute " + options.pageName());
+        //打开Flutter 页面时调用到此处 1.Native 打开 Flutter 2. Flutter 打开 Flutter
+        Intent intent = new FlutterBoostActivity.CachedEngineIntentBuilder(FlutterBoostActivity.class)
+                .backgroundMode(FlutterActivityLaunchConfigs.BackgroundMode.transparent)
+                .destroyEngineWithActivity(false)
+                .uniqueId(options.uniqueId())
+                .url(options.pageName())
+                .urlParams(options.arguments())
+                .build(FlutterBoost.instance().currentActivity());
+        FlutterBoost.instance().currentActivity().startActivity(intent);
+    }
+
+    @Override
+    public boolean popRoute(FlutterBoostRouteOptions options) {
+        //自定义popRoute处理逻辑,如果不想走默认处理逻辑返回true进行拦截
+        Toast.makeText(FlutterBoost.instance().currentActivity().getApplicationContext(), "Add customized popRoute handler here", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+}
